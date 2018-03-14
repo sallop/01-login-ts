d2fc9d1737085fb78cecd249d3efa39cd7996e43
sallop <shiogai1987@yahoo.co.jp>
2018-03-14 17:50:09 +0900

    fix: style properties for div attribute

--- src/Callback/Callback.tsx
+++ src/Callback/Callback.tsx
@@ -10,22 +10,22 @@ interface State {
 
 class Callback extends React.Component<Props, State> {
   render() {
-    // const style = {
-    //   position: 'absolute',
-    //   display: 'flex',
-    //   justifyContent: 'center',
-    //   height: '100vh',
-    //   width: '100vw',
-    //   top: 0,
-    //   bottom: 0,
-    //   left: 0,
-    //   right: 0,
-    //   backgroundColor: 'white',
-    // }
+    // https://github.com/Microsoft/TypeScript/issues/16542
+    const style: React.CSSProperties = {
+      position: 'absolute',
+      display: 'flex',
+      justifyContent: 'center',
+      height: '100vh',
+      width: '100vw',
+      top: 0,
+      bottom: 0,
+      left: 0,
+      right: 0,
+      backgroundColor: 'white',
+    };
 
-    // <div style={style}>
     return (
-      <div>
+      <div style={style}>
         <img src={loading} alt="loading"/>
       </div>
     );
